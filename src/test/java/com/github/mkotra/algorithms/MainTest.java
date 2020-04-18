cf351c2a8f0876d56bb242f0e072f56e85c909df
Michal <kotra.michal@gmail.com>
2020-04-18 10:57:57 +0200

    junit

---
+++ src/test/java/com/github/mkotra/algorithms/MainTest.java
@@ -0,0 +1,12 @@
+package com.github.mkotra.algorithms;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    public void check() {
+        Assertions.assertEquals(10, 10);
+    }
+}
