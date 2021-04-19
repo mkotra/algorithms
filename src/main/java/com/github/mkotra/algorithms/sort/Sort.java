d1f6578c37702a974167513e071fe94b7a2195e7
Michał <kotra.michal@gmail.com>
2021-04-19 12:37:19 +0200

    Update Sort.java

--- src/main/java/com/github/mkotra/algorithms/sort/Sort.java
+++ src/main/java/com/github/mkotra/algorithms/sort/Sort.java
@@ -2,14 +2,23 @@ package com.github.mkotra.algorithms.sort;
 
 public abstract class Sort {
 
+    public static Integer[] ARRAY = new Integer[] {5, 3, 8, 0, 6, 4, 2, 7, 9, 1};
+
     public static boolean less(Integer a, Integer b) {
-        return a.compareTo(b) < 0;
+        boolean result =  a.compareTo(b) < 0;
+        if (result) {
+            System.out.println(a +  " is LESS than " + b + " ");
+        } else {
+            System.out.println(a +  " is NOT LESS than " + b + " ");
+        }
+        return result;
     }
 
-    public static void exch(Integer[] arr, int i, int j) {
-        Integer tmp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = tmp;
+    public static void exch(Integer[] a, int i, int j) {
+        System.out.println("       " + a[i] + " <--> " + a[j]);
+        Integer tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
     }
 
     public static void show(Integer[] a) {
@@ -17,7 +26,7 @@ public abstract class Sort {
         for (int i = 0; i < N; i++) {
             System.out.print(a[i] + " ");
         }
-        System.out.println();
+        System.out.println(System.lineSeparator() + "-------------------------------");
     }
 
     public static boolean isSorted(Integer[] a) {
@@ -28,5 +37,10 @@ public abstract class Sort {
             }
         }
         return true;
+    }
+
+    public static void printCurrent(Integer i, Integer[] a) {
+        System.out.printf("i=%s a[i]=%s", i, a[i]);
+        System.out.println(System.lineSeparator());
     }
 }
