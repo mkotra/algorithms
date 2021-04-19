c10c88ba758157e6e7463d952417cc1add7332ee
Michał <kotra.michal@gmail.com>
2021-04-19 12:37:45 +0200

    Update Sort1Selection.java

--- src/main/java/com/github/mkotra/algorithms/sort/Sort1Selection.java
+++ src/main/java/com/github/mkotra/algorithms/sort/Sort1Selection.java
@@ -4,6 +4,7 @@ public class Sort1Selection extends Sort {
     public static void sort(Integer[] a) {
         int N = a.length;
         for (int i = 0; i < N; i++) {
+            printCurrent(i, a);
             int min = i;
             for (int j = i + 1; j < N; j++) {
                 if (less(a[j], a[min])) {
