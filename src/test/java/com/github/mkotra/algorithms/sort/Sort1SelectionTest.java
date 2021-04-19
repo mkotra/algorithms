1eccb878427a6e398715efc7cf9fa58725acaaa9
Michał <kotra.michal@gmail.com>
2021-04-19 12:39:41 +0200

    Update Sort1SelectionTest.java

--- src/test/java/com/github/mkotra/algorithms/sort/Sort1SelectionTest.java
+++ src/test/java/com/github/mkotra/algorithms/sort/Sort1SelectionTest.java
@@ -4,12 +4,14 @@ package com.github.mkotra.algorithms.sort;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static com.github.mkotra.algorithms.sort.Sort.ARRAY;
+
 class Sort1SelectionTest {
 
     @Test
     public void test() {
-        Integer[] array = new Integer[] {5, 3, 8, 0, 2, 4, 6, 7, 9, 1};
-        Sort1Selection.sort(array);
-        Assertions.assertThat(Sort1Selection.isSorted(array)).isTrue();
+        Sort1Selection.show(ARRAY);
+        Sort1Selection.sort(ARRAY);
+        Assertions.assertThat(Sort1Selection.isSorted(ARRAY)).isTrue();
     }
 }
