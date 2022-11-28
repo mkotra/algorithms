a576d49d0914ba7fb4c6390c9f740ff61ed2a569
mkotra <kotra.michal@gmail.com>
2022-11-28 12:38:19 +0100

    bump libs

--- src/test/java/com/github/mkotra/algorithms/sort/Sort2InsertionTest.java
+++ src/test/java/com/github/mkotra/algorithms/sort/Sort2InsertionTest.java
@@ -5,12 +5,12 @@ import org.junit.jupiter.api.Test;
 import static com.github.mkotra.algorithms.sort.Sort.ARRAY;
 import static org.assertj.core.api.Assertions.assertThat;
 
-class Sort1SelectionTest {
+class Sort2InsertionTest {
 
     @Test
     public void test() {
-        Sort1Selection.show(ARRAY);
-        Sort1Selection.sort(ARRAY);
+        Sort2Insertion.show(ARRAY);
+        Sort2Insertion.sort(ARRAY);
         assertThat(Sort1Selection.isSorted(ARRAY)).isTrue();
     }
 }
